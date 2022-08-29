6fcd2e01196f83c2a4cc77c1348b2fae302a46bd
Alex van Winkel <alexicoo@gmail.com>
2022-08-29 05:49:39 -0400

    initial commit

---
+++ helper.py
@@ -0,0 +1,19 @@
+def decoreer(tekst=""):
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
+    print()
+
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag/personen
+    except:
+        bedrag_pp = "??"
+    return f"Het bedrag per persoon is: {bedrag_pp} euro"
+
+b = int(input("Welk bedrag zit er in de fooienpot? "))
+p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
+
+print(fooi_pp(b,p))
